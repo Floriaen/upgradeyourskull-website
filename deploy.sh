4ef76ce3070f01b9db2fbfc34888d986ec06999d
Florian <floriaen@gmail.com>
2026-01-15 08:22:30 +0100

    Remove unused nixpacks.toml and deploy.sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo 'Building assets...'
-yarn build
-echo 'Synchronizing assets with server...'
-rsync -az --force --delete --progress public/ floriaen@213.165.72.64:/var/www/upgradeyourskull
-
-# next step https://mxd.codes/articles/how-to-deploy-your-gatsby-site-on-your-own-server
